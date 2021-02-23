44b38ed45a59fe4b9c632418c9f35a07d219d3af
Yuri Silva <y.kelviny@gmail.com>
2021-02-23 19:45:30 -0300

    Add font preload

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import { Provider } from 'react-redux';
+import Link from 'next/link';
 
 import store from '../store';
 
@@ -8,6 +9,30 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <GlobalStyle />
+      <link
+        rel="preload"
+        href="/fonts/Montserrat/Montserrat-Bold.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Montserrat/Montserrat-Regular.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Montserrat/Montserrat-Medium.ttf"
+        as="font"
+        crossOrigin=""
+      />
+      <link
+        rel="preload"
+        href="/fonts/Montserrat/Montserrat-SemiBold.ttf"
+        as="font"
+        crossOrigin=""
+      />
       <Provider store={store}>
         <Component {...pageProps} />
       </Provider>
